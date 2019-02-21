3199a25ce1415b029963b1b4b4dd5ec53dffed08
Joonas Loppi <joonas@joonas.fi>
2019-02-21 11:43:42 +0200

    upgrade buildkit & Turbo Bob

--- bin/build.sh
+++ bin/build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -eu
 
+if [ ! -L "/usr/local/bin/bup" ]; then
+	ln -s /go/src/github.com/function61/bup/rel/bup_linux-amd64 /usr/local/bin/bup
+fi
+
 source /build-common.sh
 
 BINARY_NAME="bup"
 COMPILE_IN_DIRECTORY="cmd/bup"
-# BINTRAY_PROJECT="function61/bup"
 
 # vendor dir contains non-gofmt code..
 GOFMT_TARGETS="cmd/ pkg/"
-
-# INCLUDE_WINDOWS="true"
 
 standardBuildProcess
--- build-default.Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM fn61/buildkit-golang:20181204_1302_5eedb86addc826e7
-
-WORKDIR /go/src/github.com/function61/bup
-
-CMD bin/build.sh
-
-RUN ln -s /go/src/github.com/function61/bup/rel/bup_linux-amd64 /usr/local/bin/bup
