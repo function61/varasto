5b01d431827a6b12b3f471c485052c063a473ab3
Joonas Loppi <joonas@joonas.fi>
2019-03-21 12:51:25 +0200

    commandhandlers: use helper for simplifying DB boilerplate

--- pkg/varastoserver/commandhandlers.go
+++ pkg/varastoserver/commandhandlers.go
@@ -24,434 +24,290 @@ type cHandlers struct {
 }
 
 func (c *cHandlers) VolumeCreate(cmd *VolumeCreate, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		allVolumes := []varastotypes.Volume{}
+		if err := VolumeRepository.Each(volumeAppender(&allVolumes), tx); err != nil {
+			return err
+		}
 
-	allVolumes := []varastotypes.Volume{}
-	if err := VolumeRepository.Each(volumeAppender(&allVolumes), tx); err != nil {
-		return err
-	}
-
-	if err := VolumeRepository.Update(&varastotypes.Volume{
-		ID:    len(allVolumes) + 1,
-		UUID:  varastoutils.NewVolumeUuid(),
-		Label: cmd.Name,
-		Quota: mebibytesToBytes(cmd.Quota),
-	}, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+		return VolumeRepository.Update(&varastotypes.Volume{
+			ID:    len(allVolumes) + 1,
+			UUID:  varastoutils.NewVolumeUuid(),
+			Label: cmd.Name,
+			Quota: mebibytesToBytes(cmd.Quota),
+		}, tx)
+	})
 }
 
 func (c *cHandlers) VolumeChangeQuota(cmd *VolumeChangeQuota, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		vol, err := QueryWithTx(tx).Volume(cmd.Id)
+		if err != nil {
+			return err
+		}
 
-	vol, err := QueryWithTx(tx).Volume(cmd.Id)
-	if err != nil {
-		return err
-	}
+		vol.Quota = mebibytesToBytes(cmd.Quota)
 
-	vol.Quota = mebibytesToBytes(cmd.Quota)
-
-	if err := VolumeRepository.Update(vol, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+		return VolumeRepository.Update(vol, tx)
+	})
 }
 
 func (c *cHandlers) VolumeChangeDescription(cmd *VolumeChangeDescription, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		vol, err := QueryWithTx(tx).Volume(cmd.Id)
+		if err != nil {
+			return err
+		}
 
-	vol, err := QueryWithTx(tx).Volume(cmd.Id)
-	if err != nil {
-		return err
-	}
+		vol.Description = cmd.Description
 
-	vol.Description = cmd.Description
-
-	if err := VolumeRepository.Update(vol, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+		return VolumeRepository.Update(vol, tx)
+	})
 }
 
 // FIXME: name ends in 2 because conflicts with types.VolumeMount
 func (c *cHandlers) VolumeMount2(cmd *VolumeMount2, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		vol, err := QueryWithTx(tx).Volume(cmd.Id)
+		if err != nil {
+			return err
+		}
 
-	vol, err := QueryWithTx(tx).Volume(cmd.Id)
-	if err != nil {
-		return err
-	}
+		// TODO: grab driver instance by this spec?
+		mountSpec := &varastotypes.VolumeMount{
+			ID:         varastoutils.NewVolumeMountId(),
+			Volume:     vol.ID,
+			Node:       c.conf.SelfNode.ID,
+			Driver:     varastotypes.VolumeDriverKindLocalFs,
+			DriverOpts: cmd.DriverOpts,
+		}
 
-	// TODO: grab driver instance by this spec?
-	mountSpec := &varastotypes.VolumeMount{
-		ID:         varastoutils.NewVolumeMountId(),
-		Volume:     vol.ID,
-		Node:       c.conf.SelfNode.ID,
-		Driver:     varastotypes.VolumeDriverKindLocalFs,
-		DriverOpts: cmd.DriverOpts,
-	}
+		// try mounting the volume
+		mount := blobdriver.NewLocalFs(vol.UUID, mountSpec.DriverOpts, nil)
+		if err := mount.Mountable(); err != nil {
+			return err
+		}
 
-	// try mounting the volume
-	mount := blobdriver.NewLocalFs(vol.UUID, mountSpec.DriverOpts, nil)
-	if err := mount.Mountable(); err != nil {
-		return err
-	}
-
-	if err := VolumeMountRepository.Update(mountSpec, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+		return VolumeMountRepository.Update(mountSpec, tx)
+	})
 }
 
 func (c *cHandlers) VolumeUnmount(cmd *VolumeUnmount, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		mount, err := QueryWithTx(tx).VolumeMount(cmd.Id)
+		if err != nil {
+			return err
+		}
 
-	mount, err := QueryWithTx(tx).VolumeMount(cmd.Id)
-	if err != nil {
-		return err
-	}
-
-	if err := VolumeMountRepository.Delete(mount, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+		return VolumeMountRepository.Delete(mount, tx)
+	})
 }
 
 func (c *cHandlers) DirectoryCreate(cmd *DirectoryCreate, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
-
-	if err := DirectoryRepository.Update(&varastotypes.Directory{
-		ID:     varastoutils.NewDirectoryId(),
-		Parent: cmd.Parent,
-		Name:   cmd.Name,
-	}, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		return DirectoryRepository.Update(&varastotypes.Directory{
+			ID:     varastoutils.NewDirectoryId(),
+			Parent: cmd.Parent,
+			Name:   cmd.Name,
+		}, tx)
+	})
 }
 
 func (c *cHandlers) DirectoryDelete(cmd *DirectoryDelete, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		dir, err := QueryWithTx(tx).Directory(cmd.Id)
+		if err != nil {
+			return err
+		}
 
-	dir, err := QueryWithTx(tx).Directory(cmd.Id)
-	if err != nil {
-		return err
-	}
+		collections, err := QueryWithTx(tx).CollectionsByDirectory(dir.ID)
+		if err != nil {
+			return err
+		}
 
-	collections, err := QueryWithTx(tx).CollectionsByDirectory(dir.ID)
-	if err != nil {
-		return err
-	}
+		subDirs, err := QueryWithTx(tx).SubDirectories(dir.ID)
+		if err != nil {
+			return err
+		}
 
-	subDirs, err := QueryWithTx(tx).SubDirectories(dir.ID)
-	if err != nil {
-		return err
-	}
+		if len(collections) > 0 {
+			return fmt.Errorf("Cannot delete directory because it has %d collection(s)", len(collections))
+		}
 
-	if len(collections) > 0 {
-		return fmt.Errorf("Cannot delete directory because it has %d collection(s)", len(collections))
-	}
+		if len(subDirs) > 0 {
+			return fmt.Errorf("Cannot delete directory because it has %d directory(s)", len(subDirs))
+		}
 
-	if len(subDirs) > 0 {
-		return fmt.Errorf("Cannot delete directory because it has %d directory(s)", len(subDirs))
-	}
-
-	if err := DirectoryRepository.Delete(dir, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+		return DirectoryRepository.Delete(dir, tx)
+	})
 }
 
 func (c *cHandlers) DirectoryRename(cmd *DirectoryRename, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		dir, err := QueryWithTx(tx).Directory(cmd.Id)
+		if err != nil {
+			return err
+		}
 
-	dir, err := QueryWithTx(tx).Directory(cmd.Id)
-	if err != nil {
-		return err
-	}
+		dir.Name = cmd.Name
 
-	dir.Name = cmd.Name
-
-	if err := DirectoryRepository.Update(dir, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+		return DirectoryRepository.Update(dir, tx)
+	})
 }
 
 func (c *cHandlers) DirectoryChangeDescription(cmd *DirectoryChangeDescription, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		dir, err := QueryWithTx(tx).Directory(cmd.Id)
+		if err != nil {
+			return err
+		}
 
-	dir, err := QueryWithTx(tx).Directory(cmd.Id)
-	if err != nil {
-		return err
-	}
+		dir.Description = cmd.Description
 
-	dir.Description = cmd.Description
-
-	if err := DirectoryRepository.Update(dir, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+		return DirectoryRepository.Update(dir, tx)
+	})
 }
 
 func (c *cHandlers) DirectoryChangeSensitivity(cmd *DirectoryChangeSensitivity, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		dir, err := QueryWithTx(tx).Directory(cmd.Id)
+		if err != nil {
+			return err
+		}
 
-	dir, err := QueryWithTx(tx).Directory(cmd.Id)
-	if err != nil {
-		return err
-	}
+		dir.Sensitivity = cmd.Sensitivity
 
-	dir.Sensitivity = cmd.Sensitivity
-
-	if err := DirectoryRepository.Update(dir, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+		return DirectoryRepository.Update(dir, tx)
+	})
 }
 
 func (c *cHandlers) DirectoryMove(cmd *DirectoryMove, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		dirToMove, err := QueryWithTx(tx).Directory(cmd.Id)
+		if err != nil {
+			return err
+		}
 
-	dirToMove, err := QueryWithTx(tx).Directory(cmd.Id)
-	if err != nil {
-		return err
-	}
+		// verify that new parent exists
+		newParent, err := QueryWithTx(tx).Directory(cmd.Directory)
+		if err != nil {
+			return err
+		}
 
-	// verify that new parent exists
-	newParent, err := QueryWithTx(tx).Directory(cmd.Directory)
-	if err != nil {
-		return err
-	}
+		if dirToMove.ID == newParent.ID {
+			return errors.New("dir cannot be its own parent, dawg")
+		}
 
-	if dirToMove.ID == newParent.ID {
-		return errors.New("dir cannot be its own parent, dawg")
-	}
+		dirToMove.Parent = newParent.ID
 
-	dirToMove.Parent = newParent.ID
-
-	if err := DirectoryRepository.Update(dirToMove, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+		return DirectoryRepository.Update(dirToMove, tx)
+	})
 }
 
 func (c *cHandlers) CollectionMove(cmd *CollectionMove, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		// check for existence
+		if _, err := QueryWithTx(tx).Directory(cmd.Directory); err != nil {
+			return err
+		}
 
-	// check for existence
-	_, err = QueryWithTx(tx).Directory(cmd.Directory)
-	if err != nil {
-		return err
-	}
+		coll, err := QueryWithTx(tx).Collection(cmd.Collection)
+		if err != nil {
+			return err
+		}
 
-	coll, err := QueryWithTx(tx).Collection(cmd.Collection)
-	if err != nil {
-		return err
-	}
+		coll.Directory = cmd.Directory
 
-	coll.Directory = cmd.Directory
-
-	if err := CollectionRepository.Update(coll, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+		return CollectionRepository.Update(coll, tx)
+	})
 }
 
 func (c *cHandlers) CollectionChangeDescription(cmd *CollectionChangeDescription, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		coll, err := QueryWithTx(tx).Collection(cmd.Collection)
+		if err != nil {
+			return err
+		}
 
-	coll, err := QueryWithTx(tx).Collection(cmd.Collection)
-	if err != nil {
-		return err
-	}
+		coll.Description = cmd.Description
 
-	coll.Description = cmd.Description
-
-	if err := CollectionRepository.Update(coll, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+		return CollectionRepository.Update(coll, tx)
+	})
 }
 
 func (c *cHandlers) CollectionRename(cmd *CollectionRename, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		coll, err := QueryWithTx(tx).Collection(cmd.Collection)
+		if err != nil {
+			return err
+		}
 
-	coll, err := QueryWithTx(tx).Collection(cmd.Collection)
-	if err != nil {
-		return err
-	}
+		coll.Name = cmd.Name
 
-	coll.Name = cmd.Name
-
-	if err := CollectionRepository.Update(coll, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+		return CollectionRepository.Update(coll, tx)
+	})
 }
 
 func (c *cHandlers) CollectionDelete(cmd *CollectionDelete, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		coll, err := QueryWithTx(tx).Collection(cmd.Collection)
+		if err != nil {
+			return err
+		}
 
-	coll, err := QueryWithTx(tx).Collection(cmd.Collection)
-	if err != nil {
-		return err
-	}
+		if cmd.Name != coll.Name {
+			return fmt.Errorf("repeated name incorrect, expecting %s", coll.Name)
+		}
 
-	if cmd.Name != coll.Name {
-		return fmt.Errorf("repeated name incorrect, expecting %s", coll.Name)
-	}
-
-	if err := CollectionRepository.Delete(coll, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+		return CollectionRepository.Delete(coll, tx)
+	})
 }
 
 func (c *cHandlers) ClientCreate(cmd *ClientCreate, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
-
-	if err := ClientRepository.Update(&varastotypes.Client{
-		ID:        varastoutils.NewClientId(),
-		Name:      cmd.Name,
-		AuthToken: cryptorandombytes.Base64Url(32),
-	}, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		return ClientRepository.Update(&varastotypes.Client{
+			ID:        varastoutils.NewClientId(),
+			Name:      cmd.Name,
+			AuthToken: cryptorandombytes.Base64Url(32),
+		}, tx)
+	})
 }
 
 func (c *cHandlers) ClientRemove(cmd *ClientRemove, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
-
-	if err := ClientRepository.Delete(&varastotypes.Client{
-		ID: cmd.Id,
-	}, tx); err != nil {
-		return err
-	}
-
-	return tx.Commit()
+	return c.db.Update(func(tx *bolt.Tx) error {
+		return ClientRepository.Delete(&varastotypes.Client{
+			ID: cmd.Id,
+		}, tx)
+	})
 }
 
 func (c *cHandlers) ReplicationpolicyChangeDesiredVolumes(cmd *ReplicationpolicyChangeDesiredVolumes, ctx *command.Ctx) error {
-	tx, err := c.db.Begin(true)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
-
-	desiredVolumes := []int{}
-	if err := json.Unmarshal([]byte(cmd.DesiredVolumes), &desiredVolumes); err != nil {
-		return err
-	}
-
-	if len(desiredVolumes) < 1 {
-		return errors.New("need at least 1 volume")
-	}
-
-	// verify that each volume exists
-	for _, desiredVolume := range desiredVolumes {
-		if _, err := QueryWithTx(tx).Volume(desiredVolume); err != nil {
-			return fmt.Errorf("desiredVolume %d: %v", desiredVolume, err)
+	return c.db.Update(func(tx *bolt.Tx) error {
+		desiredVolumes := []int{}
+		if err := json.Unmarshal([]byte(cmd.DesiredVolumes), &desiredVolumes); err != nil {
+			return err
 		}
-	}
 
-	policy, err := QueryWithTx(tx).ReplicationPolicy(cmd.Id)
-	if err != nil {
-		return err
-	}
+		if len(desiredVolumes) < 1 {
+			return errors.New("need at least 1 volume")
+		}
 
-	policy.DesiredVolumes = desiredVolumes
+		// verify that each volume exists
+		for _, desiredVolume := range desiredVolumes {
+			if _, err := QueryWithTx(tx).Volume(desiredVolume); err != nil {
+				return fmt.Errorf("desiredVolume %d: %v", desiredVolume, err)
+			}
+		}
 
-	if err := ReplicationPolicyRepository.Update(policy, tx); err != nil {
-		return err
-	}
-	return tx.Commit()
+		policy, err := QueryWithTx(tx).ReplicationPolicy(cmd.Id)
+		if err != nil {
+			return err
+		}
+
+		policy.DesiredVolumes = desiredVolumes
+
+		return ReplicationPolicyRepository.Update(policy, tx)
+	})
 }
 
 func registerCommandEndpoints(
