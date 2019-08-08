00e40929b6b4686e844c9933116bf4dee562c5d0
Joonas Loppi <joonas@joonas.fi>
2019-08-08 17:20:59 +0300

    stofuse: we don't need the mount/unmount verbs

--- pkg/stofuse/entrypoint_notwin.go
+++ pkg/stofuse/entrypoint_notwin.go
@@ -5,7 +5,6 @@ package stofuse
 import (
 	"github.com/function61/gokit/ossignal"
 	"github.com/function61/gokit/stopper"
-	"github.com/function61/varasto/pkg/stofuse/stofuseclient"
 	"github.com/spf13/cobra"
 	"log"
 )
@@ -15,28 +14,6 @@ func Entrypoint() *cobra.Command {
 		Use:   "fuse",
 		Short: "Varasto-FUSE integration",
 	}
-
-	cmd.AddCommand(&cobra.Command{
-		Use:   "mount <collectionId>",
-		Short: "Mounts a collection by id",
-		Args:  cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			if err := stofuseclient.New().Mount(args[0]); err != nil {
-				panic(err)
-			}
-		},
-	})
-
-	cmd.AddCommand(&cobra.Command{
-		Use:   "unmount <collectionId>",
-		Short: "Unmounts a collection by id",
-		Args:  cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			if err := stofuseclient.New().Unmount(args[0]); err != nil {
-				panic(err)
-			}
-		},
-	})
 
 	cmd.AddCommand(&cobra.Command{
 		Use:   "serve",
