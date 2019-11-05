1e66a2264bc97df4b14984e9dd042ac7600e443b
Joonas Loppi <joonas@joonas.fi>
2019-11-05 11:55:40 +0200

    stomvu: move TV stuff to own file

--- pkg/stomvu/entrypoint.go
+++ pkg/stomvu/entrypoint.go
@@ -1,10 +1,7 @@
 package stomvu
 
 import (
-	"github.com/function61/varasto/pkg/seasonepisodedetector"
 	"github.com/spf13/cobra"
-	"os"
-	"path/filepath"
 )
 
 func Entrypoint() *cobra.Command {
@@ -13,45 +10,11 @@ func Entrypoint() *cobra.Command {
 		Short: `Renaming utils ("mv utils") for photos, TV series etc.`,
 	}
 
-	cmd.AddCommand(epEntrypoint())
+	cmd.AddCommand(tvEntrypoint())
 	cmd.AddCommand(photoEntrypoint())
 	cmd.AddCommand(customMonthlyPatternEntrypoint())
 
 	return cmd
-}
-
-func epEntrypoint() *cobra.Command {
-	doIt := false
-
-	cmd := &cobra.Command{
-		Use:   "tv",
-		Short: "Renames TV episodes",
-		Args:  cobra.NoArgs,
-		Run: func(cmd *cobra.Command, args []string) {
-			plan, err := computePlan(episodeFromFilename)
-			panicIfError(err)
-
-			if doIt {
-				panicIfError(executePlan(plan))
-			} else {
-				explainPlan(plan, os.Stdout)
-			}
-		},
-	}
-
-	cmd.Flags().BoolVarP(&doIt, "do", "", doIt, "Whether to execute the plan or run a dry run")
-
-	return cmd
-}
-
-func episodeFromFilename(input string) string {
-	result := seasonepisodedetector.Detect(input)
-	if result == nil {
-		return ""
-	}
-
-	// "S03/S03E07"
-	return filepath.Join(result.SeasonDesignation(), result.String())
 }
 
 func panicIfError(err error) {
---
+++ pkg/stomvu/tv.go
@@ -0,0 +1,42 @@
+package stomvu
+
+import (
+	"github.com/function61/varasto/pkg/seasonepisodedetector"
+	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
+)
+
+func tvEntrypoint() *cobra.Command {
+	doIt := false
+
+	cmd := &cobra.Command{
+		Use:   "tv",
+		Short: "Renames TV episodes",
+		Args:  cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			plan, err := computePlan(episodeFromFilename)
+			panicIfError(err)
+
+			if doIt {
+				panicIfError(executePlan(plan))
+			} else {
+				explainPlan(plan, os.Stdout)
+			}
+		},
+	}
+
+	cmd.Flags().BoolVarP(&doIt, "do", "", doIt, "Whether to execute the plan or run a dry run")
+
+	return cmd
+}
+
+func episodeFromFilename(input string) string {
+	result := seasonepisodedetector.Detect(input)
+	if result == nil {
+		return ""
+	}
+
+	// "S03/S03E07"
+	return filepath.Join(result.SeasonDesignation(), result.String())
+}
