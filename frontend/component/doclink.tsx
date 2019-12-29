48ee801e73830decc753f89d46d9d680e83455ef
Joonas Loppi <joonas@joonas.fi>
2019-12-29 13:33:46 +0200

    doclink: use f61ui/component/docslink

--- frontend/component/doclink.tsx
+++ frontend/component/doclink.tsx
@@ -1,3 +1,4 @@
+import { DocsLink } from 'f61ui/component/docslink';
 import { DocRef } from 'generated/stoserver/stoservertypes_types';
 import * as React from 'react';
 
@@ -5,17 +6,12 @@ interface DocLinkProps {
 	doc: DocRef;
 	title?: string;
 }
-
 export class DocLink extends React.Component<DocLinkProps, {}> {
 	render() {
-		return (
-			<a
-				href={'https://github.com/function61/varasto/blob/master/' + this.props.doc}
-				title={this.props.title || 'View documentation'}
-				target="_blank">
-				<span className="glyphicon glyphicon-question-sign">&nbsp;</span>
-				{this.props.title || ''}
-			</a>
-		);
+		return <DocsLink url={DocGitHubMaster(this.props.doc)} title={this.props.title} />;
 	}
+}
+
+export function DocGitHubMaster(doc: DocRef): string {
+	return 'https://github.com/function61/varasto/blob/master/' + doc;
 }
