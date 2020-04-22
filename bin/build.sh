eafdef876ad890b3b9bca95c56198c0d09449317
Joonas Loppi <joonas@joonas.fi>
2020-04-22 10:05:26 +0300

    simple start of an update server (static website with latest-version.json)

--- bin/build.sh
+++ bin/build.sh
@@ -5,9 +5,37 @@ if [ ! -L "/usr/local/bin/sto" ]; then
 fi
 
 function docsDeployerSpec {
+	if [ -n "${FASTBUILD:-}" ]; then
+		return # skip non-essential step
+	fi
+
 	cd misc/docs-website-deployerspec/
 
 	deployer package "$FRIENDLY_REV_ID" ../../rel/docs-website-deployerspec.zip
+}
+
+function updateServer {
+	if [ -n "${FASTBUILD:-}" ]; then
+		return # skip non-essential step
+	fi
+
+	cd misc/varasto-updateserver/
+
+	# this is a file that will be deployed (by function61/deployer) at:
+	#     https://function61.com/varasto/updateserver/latest-version.json
+	# this won't be deployed on all commits however - deployments will be initiated manually
+	# when we want users to update to the released version
+	echo -n "{\"LatestVersion\": \"$FRIENDLY_REV_ID\"}" > latest-version.json
+
+	tar -czf "updateserver.tar.gz" latest-version.json
+
+	# so it won't end up in deployerspec zip
+	rm latest-version.json
+
+	deployer package "$FRIENDLY_REV_ID" ../../rel/updateserver-deployerspec.zip
+
+	# clean up generated files
+	rm updateserver.tar.gz
 }
 
 # make sure parent dir exits, under which FUSE projector will mount itself
@@ -24,3 +52,5 @@ GOFMT_TARGETS="cmd/ pkg/"
 standardBuildProcess
 
 (docsDeployerSpec)
+
+(updateServer)
