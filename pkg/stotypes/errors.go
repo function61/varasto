7a8475777fed3e75905c07093d11148f8cbae915
Joonas Loppi <joonas@joonas.fi>
2020-05-11 10:35:53 +0300

    stotypes: remove unused error codes

--- pkg/stotypes/errors.go
+++ pkg/stotypes/errors.go
@@ -5,10 +5,6 @@ import (
 )
 
 var (
-	ErrChunkMetadataNotFound       = errors.New("chunk metadata not found")
-	ErrChunkAlreadyExists          = errors.New("chunk already exists")
 	ErrBlobNotAccessibleOnThisNode = errors.New("blob not accessible on this node")
-	ErrUnknownCommand              = errors.New("unknown command")
-	// ErrUnexpectedResponseStatusCode = errors.New("unexpected response code")
-	ErrBadBlobRef = errors.New("bad blob ref")
+	ErrBadBlobRef                  = errors.New("bad blob ref")
 )
